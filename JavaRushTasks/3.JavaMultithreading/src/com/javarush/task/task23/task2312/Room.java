253114fa6159bb90b148e0ff23e2527f041635e4
YSavitski <07ersh@gmail.com>
2017-02-13 19:00:25 +0300

    starting solver task2312

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task23.task2312;
+
+/**
+ * Created by esavitski on 13.02.2017.
+ */
+public class Room {
+    public static void main(String[] args){
+
+    }
+}
---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task23.task2312;
+
+/**
+ * Created by esavitski on 13.02.2017.
+ */
+public class Snake {
+}
