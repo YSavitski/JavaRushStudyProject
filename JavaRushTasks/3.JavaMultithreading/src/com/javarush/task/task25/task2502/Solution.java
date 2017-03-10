59c60e726931ef575d917dd02d15937dc5a9813d
YSavitski <07ersh@gmail.com>
2017-03-10 19:00:22 +0300

    first task from 25th

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.*;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+
+        public Car() {
+            //init wheels here
+            wheels = new ArrayList<Wheel>();
+            for (String name: loadWheelNamesFromDB()) {
+                try {
+                    wheels.add(Wheel.valueOf(name));
+                } catch (IllegalArgumentException e) {
+
+                }
+            }
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
