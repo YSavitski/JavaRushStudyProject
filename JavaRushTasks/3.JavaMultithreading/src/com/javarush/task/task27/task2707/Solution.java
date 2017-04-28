2b36ae12231b32f95154bb1b3f99e482659d5d55
Yauheni Savitski <Yauheni Savitski>
2017-04-29 01:42:48 +0300

    add others tasks from 27th level

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
@@ -6,35 +6,15 @@ package com.javarush.task.task27.task2707;
 public class Solution {
     public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
         synchronized (obj1) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ignored) {
-            }
             synchronized (obj2) {
                 System.out.println(obj1 + " " + obj2);
             }
         }
     }
+
     public static boolean isNormalLockOrder(final Solution solution, final Object o1, final Object o2) throws Exception {
         //do something here
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-                solution.someMethodWithSynchronizedBlocks(o1, o2);
-            }
-        };
-
-        Object objMax = o1.hashCode() > o2.hashCode() ? o1 : o2;
-        Object objMin = o1.hashCode() > o2.hashCode() ? o2 : o1;
-
-        synchronized (objMax){
-            synchronized (objMin) {
-                thread.start();
-                Thread.sleep(10);
-                System.out.println(thread.getState().toString());
-                return thread.getState()== Thread.State.BLOCKED;
-            }
-        }
+        return false;
     }
 
     public static void main(String[] args) throws Exception {
